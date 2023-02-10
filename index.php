810b7faee3dacb1840be26c9af3b71d3480667aa
softeam2 <softeam2@FRP02448.softeam.com>
2023-02-10 10:21:17 +0100

    commit num2

--- index.php
+++ index.php
@@ -6,7 +6,7 @@ include_once ROOT . 'views/includes/header.php'; ?>
 
 <body>
     <?php include_once ROOT . 'views/includes/navbar.php'; ?>
-    <p>vente tel</p>
+    <p>vente tel for all</p>
     <?php include_once ROOT . 'views/includes/footer.php'; ?>
 </body>
 
